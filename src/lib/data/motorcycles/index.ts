b91286c5a0a35118158b1fc261ea836ff26890e8
JeanPierreAntunes <jeanpierredacostaantunes@gmail.com>
2026-05-02 14:41:04 +0100

    arrumar nomes de pastas

--- src/lib/data/motorcycles/index.ts
+++ src/lib/data/motorcycles/index.ts
@@ -61,7 +61,7 @@ import apriliaSR_GT_Sport_125 from './Aprilia/SR/sr_gt_sport_125.json';
 import apriliaSX_125 from './Aprilia/SX/SX_125.json';
 
 // SXR
-import apriliaSXR_50 from './Aprilia/SXR/sxr.json';
+import apriliaSXR_50 from './Aprilia/sxr/sxr.json';
 
 // TUAREG
 import apriliaTuareg_660 from './Aprilia/Tuareg/Tuareg_660.json';
